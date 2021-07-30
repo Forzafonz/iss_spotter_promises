a69435be161f61af94fe06a16d592eeb9f7d3821
Anton <golubin1990@gmail.com>
2021-07-29 20:44:17 -0400

    Updated the code to use promises chained together

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const { nextISSTimesForMyLocation } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes} = require('./iss');
 
-const printResults = function(err, data) {
-  if (err) {
-    console.log("Something went wrong: ", err);
-  } else {
-    for (const pass of data) {
-      let timestamp = pass['risetime'];
-      let date = new Date(0);
-      date.setUTCSeconds(timestamp);
-      console.log(`Next pass at ${date} for ${pass['duration']}`);
-    }
+const printResults = function(data) {
+  for (const pass of data) {
+    let timestamp = pass['risetime'];
+    let date = new Date(0);
+    date.setUTCSeconds(timestamp);
+    console.log(`Next pass at ${date} for ${pass['duration']}`);
   }
 };
 
-nextISSTimesForMyLocation(printResults);
+const IP = fetchMyIP();
+
+IP
+.then((result) => {
+  let ip = JSON.parse(result).ip;
+  return ip;
+})
+.then(fetchCoordsByIP)
+.then((body) => {
+  let {latitude, longitude} = JSON.parse(body)
+  return {latitude, longitude}
+})
+.then(fetchISSFlyOverTimes)
+.then((body) => {
+  let data = JSON.parse(body).response;
+  return (data)
+})
+.then(printResults)
